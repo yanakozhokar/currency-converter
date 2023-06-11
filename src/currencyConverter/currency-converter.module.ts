71d9ec9155a2ec3eb736951230d407590fb35dc4
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-06-11 13:42:55 +0300

    fix: fix mistake

--- src/currencyConverter/currency-converter.module.ts
+++ src/currencyConverter/currency-converter.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CurrencyConverterComponent } from './currency-converter.component';
 
 @NgModule({
-  declarations: [
-    CurrencyConverterComponent
-  ],
   imports: [
     CommonModule,
     FormsModule
   ],
-  exports: [
-    CurrencyConverterComponent
-  ]
+  exports: []
 })
 export class CurrencyConverterModule { }
